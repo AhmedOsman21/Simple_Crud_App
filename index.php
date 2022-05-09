0d0a09beebe2fa80097a602b9db39fe9ec718c36
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-05-09 05:39:11 +0200

    Make the structure of records' table page

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>CRUD Application</title>
+</head>
+
+<body>
+
+    <div class="container main-container">
+        <h1>Crud Application</h1>
+
+        <table class="table table-secondary table-striped">
+            <tr class="table-dark">
+                <th style="width: 10%">ID</th>
+                <th>Username</th>
+                <th>Name</th>
+                <th>Birth Date</th>
+                <th style="width: 30%">Options</th>
+            </tr>
+
+
+            <tr>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+
+            <tr>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+            <tr>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+
+            <tr>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+
+            <tr>
+
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+
+            <tr>
+
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+
+            <tr>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+        </table>
+
+    </div>
+
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
