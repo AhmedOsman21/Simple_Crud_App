84a860998f0a079d5fada788074a92bb777741dc
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-05-09 06:37:37 +0200

    Adding buttons & testing dummy data

--- index.php
+++ index.php
@@ -14,75 +14,77 @@
 
     <div class="container main-container">
         <h1>Crud Application</h1>
-
+        
+        
         <table class="table table-secondary table-striped">
             <tr class="table-dark">
                 <th style="width: 10%">ID</th>
                 <th>Username</th>
                 <th>Name</th>
-                <th>Birth Date</th>
-                <th style="width: 30%">Options</th>
+                <th>Email</th>
+                <th style="width: 20%">Options</th>
             </tr>
-
-
+            
+            
             <tr>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
+                <td>1</td>
+                <td>adamkr213</td>
+                <td>Adam Darwen</td>
+                <td>adamdarwen213@gmail.com</td>
+                <td class="opt-field"><button class="btn btn-primary">Update</button> <button class="btn btn-danger">Delete</button></td>
             </tr>
-
+            
             <tr>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
+                <td>2</td>
+                <td>johntayson1</td>
+                <td>Jason Tayson</td>
+                <td>johnreko42@gmail.com</td>
+                <td class="opt-field"><button class="btn btn-primary">Update</button> <button class="btn btn-danger">Delete</button></td>
             </tr>
             <tr>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
+                <td>3</td>
+                <td>carlosawen</td>
+                <td>Carl Sandeel</td>
+                <td>carlsawen@gmail.com</td>
+                <td class="opt-field"><button class="btn btn-primary">Update</button> <button class="btn btn-danger">Delete</button></td>
             </tr>
-
+            
             <tr>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-            </tr>
-
-            <tr>
-
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
+                <td>4</td>
+                <td>ladderclimber23</td>
+                <td>Jacklen Daher</td>
+                <td>jacklendaher@gmail.com</td>
+                <td class="opt-field"><button class="btn btn-primary">Update</button> <button class="btn btn-danger">Delete</button></td>
             </tr>
 
             <tr>
 
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
+                <td>5</td>
+                <td>savytech103</td>
+                <td>Tareq Fathy</td>
+                <td>tf2olhamed@gmail.com</td>
+                <td class="opt-field"><button class="btn btn-primary">Update</button> <button class="btn btn-danger">Delete</button></td>
             </tr>
 
             <tr>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
-                <td>...</td>
+                
+                <td>6</td>
+                <td>johnsmith123</td>
+                <td>John Smith</td>
+                <td>joesmthl22@outlook.com</td>
+                <td class="opt-field"><button class="btn btn-primary">Update</button> <button class="btn btn-danger">Delete</button></td>
+            </tr>
+
+            <tr>
+                <td>7</td>
+                <td>sandrawh1t3</td>
+                <td>Sandra White</td>
+                <td>sansansun@gmail.com</td>
+                <td class="opt-field"><button class="btn btn-primary">Update</button> <button class="btn btn-danger">Delete</button></td>
             </tr>
         </table>
 
+        <button class="btn btn-success new-rec">  <img src="icons/new-rec.png" width="15" alt="" style="margin-right: 2px; margin-bottom: 3px">New Record</button>
     </div>
 
     <script src="js/bootstrap.min.js"></script>
