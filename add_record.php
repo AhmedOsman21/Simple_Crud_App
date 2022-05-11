d0db6fd8283c3569de7683e4c936d72a04812254
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-05-11 15:16:01 +0200

    Made a page to recieve data from user to add records

---
+++ add_record.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>CRUD Application</title>
+</head>
+
+<body>
+
+
+
+    <div class="container main-container">
+
+        <div class="heading" style="margin-bottom: 2rem;">
+            <h2>Crud Application</h2>
+        </div>
+
+        <div class="form-control">
+            <h4>Add new record</h4>
+            <form action="<?php htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" name="username" id="username" class="text-inp">
+                <label for="fname" class="form-label">First Name</label>
+                <input type="text" name="fname" id="fname" class="text-inp">
+                <label for="lname" class="form-label">Last Name</label>
+                <input type="text" name="lname" id="lname" class="text-inp">
+                <label for="email" class="form-label">Email Address</label>
+                <input type="text" name="email" id="email" class="text-inp">
+                <div class="submit-form">
+                    <input type="submit" value="Register" class="btn btn-success submit-btn">
+                </div>
+            </form>
+        </div>
+
+    </div>
+
+
+
+    <script type="text/javascript" src="js/script.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -101,7 +101,7 @@
     <script type = "text/javascript" src = "js/bootstrap.min.js" ></script>
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
+    
 </body>
 
 </html>
