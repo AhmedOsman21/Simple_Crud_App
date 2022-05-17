3a3c6156cff4246de40b5eff278c6db036294e18
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-05-17 21:43:02 +0200

    Created a member function to read all data from database and return it as a associative array

--- classes/DB_Class/Register.php
+++ classes/DB_Class/Register.php
@@ -1,12 +1,20 @@
 <?php
+
 namespace DB_Class;
 
 include "Db_Connection.php";
 
 
-class Records extends DB_Connection { 
+class Records extends DB_Connection {
     private $username;
     private $name;
     private $email;
-    
+
+    // Read
+    public function readAll() {
+        $sql = "SELECT * FROM users";
+        $stmt = $this->conn->query($sql);
+        $data = $stmt->fetchAll();
+        return $data;
+    }
 }
