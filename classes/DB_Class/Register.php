f34a799f1a962d4ad8e7fd018e3b198789b0fb75
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-05-19 00:29:50 +0200

    Created class setters for class properties

--- classes/DB_Class/Register.php
+++ classes/DB_Class/Register.php
@@ -6,10 +6,30 @@ include "Db_Connection.php";
 
 
 class Records extends DB_Connection {
+    private $id;
     private $username;
     private $name;
     private $email;
 
+
+    // Class Setters
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setUser($username) {
+        $this->username = $username;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    
     // Read
     public function readAll() {
         $sql = "SELECT * FROM users";
