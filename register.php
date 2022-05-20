e29c729d85f313ab4edbe7d51a485295f982cc63
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-05-20 23:58:06 +0200

    Add validation on input fields to check wether it's empty or not

--- register.php
+++ register.php
@@ -13,3 +13,28 @@ function clearInput($data) {
     $data = trim(stripslashes(htmlspecialchars($data)));
     return $data;
 }
+
+
+// Input validation
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    // Username Validation
+    if (empty($_POST['username'])) {
+        $usernameErr = "Username is required!";
+    }
+
+    // First Name Validation
+    if (empty($_POST['fname'])) {
+        $fnameErr = "First name is required!";
+    }
+
+    // Last Name Validation
+    if (empty($_POST['lname'])) {
+        $lnameErr = "Last name is required!";
+    }
+
+    // Email Validation
+    if (empty($_POST['email'])) {
+        $emailErr = "Email address is required!";
+    }
+}
