79661586320325577c50b393bdd8589d85852da1
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-05-21 01:00:47 +0200

    Check if username is valid

--- register.php
+++ register.php
@@ -1,12 +1,12 @@
 <?php
-
-
 // Error Variables
 $usernameErr = $fnameErr = $lnameErr = $emailErr = "";
 
 // Form Variables
 $username = $fname = $lname = $email = "";
 
+// Username Regex Pattern
+$userPattern = "/^\w{5,50}$/";
 
 // Clean Inputs
 function clearInput($data) {
@@ -21,6 +21,12 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     // Username Validation
     if (empty($_POST['username'])) {
         $usernameErr = "Username is required!";
+    } else {
+        // Check if username is valid
+        if (!preg_match($userPattern, $_POST['username'])) {
+            $usernameErr = "Please only use letters (a-z), numbers and underscore _";
+        }
+
     }
 
     // First Name Validation
