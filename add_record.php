0ef85d37f28867c6960c182542fa85ac36bfafab
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-05-21 03:15:22 +0200

    Imported register.php file & show errors of the form in the same page if it has errors

--- add_record.php
+++ add_record.php
@@ -1,3 +1,7 @@
+<?php
+include_once "register.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,15 +26,19 @@
 
         <div class="form-control">
             <h4>Add new record</h4>
-            <form action="register.php" method="post">
-                <label for="username" class="form-label">Username</label>
+            <form action="<?php htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
+                <label for="username" class="form-label">Username <span class="required">*</span></label>
                 <input type="text" name="username" id="username" class="text-inp">
-                <label for="fname" class="form-label">First Name</label>
+                <span class="error"><?php echo $usernameErr; ?></span> 
+                <label for="fname" class="form-label">First Name <span class="required">*</span></label>
                 <input type="text" name="fname" id="fname" class="text-inp">
-                <label for="lname" class="form-label">Last Name</label>
+                <span class="error"><?php echo $fnameErr; ?> </span> 
+                <label for="lname" class="form-label">Last Name <span class="required">*</span></label>
                 <input type="text" name="lname" id="lname" class="text-inp">
-                <label for="email" class="form-label">Email Address</label>
+                <span class="error"><?php echo $lnameErr; ?></span> 
+                <label for="email" class="form-label">Email Address <span class="required">*</span></label>
                 <input type="text" name="email" id="email" class="text-inp">
+                <span class="error"><?php echo $emailErr; ?> </span> 
                 <div class="submit-form">
                     <input type="submit" value="Register" class="btn btn-success submit-btn" name="register">
                 </div>
