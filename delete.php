0afde70b2c0a6aea9d118ef0b828d2859ad3a128
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-05-22 04:20:01 +0200

    Delete the user of the given id

--- delete.php
+++ delete.php
@@ -10,5 +10,7 @@ $record = new Record;
 if (isset($_GET['id'])) {
     if ($_GET['request'] == "delete") {
         $record->setId($_GET['id']);
+        // Delete the record of this id.
+        $record->delete();
     }
 }
