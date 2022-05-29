85727061241b58dc38cfa2b30453a820818f0ec6
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-05-29 19:55:54 +0200

    Renamed first & last name columns in users table in sql to match the naming of the code

--- db.sql
+++ db.sql
@@ -5,6 +5,6 @@ CREATE TABLE users(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
-    firstName VARCHAR(25),
-    lastName VARCHAR(25)
+    first_name VARCHAR(25),
+    last_name VARCHAR(25)
 );
